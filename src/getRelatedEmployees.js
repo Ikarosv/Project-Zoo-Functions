863a99d340e7df9532f5f9e327daf17884a8a56d
Ikarosv <ikarovieiraa@gmail.com>
2022-09-29 17:48:07 -0300

    Requisito 4 feito.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return !!data.employees.find(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return data.employees.reduce((arrFinal, currEmploye) => {
+    if (currEmploye.managers.includes(managerId)) {
+      arrFinal.push(`${currEmploye.firstName} ${currEmploye.lastName}`);
+    }
+    return arrFinal;
+  }, []);
 }
+
+// console.log(getRelatedEmployees('d'));
 
 module.exports = { isManager, getRelatedEmployees };
