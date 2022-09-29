e5c671aec66e903920583c0f4713722395860d40
Ikarosv <ikarovieiraa@gmail.com>
2022-09-29 16:09:10 -0300

    Refazendo requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,15 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   if (!ids.length) return [];
-  return ids.map((idSpecie) => data.species.filter(({ id: idAnimal }) => idAnimal === idSpecie));
+  return ids.reduce((animals, idSpecie) => animals.concat(data.species.filter(({ id: idAnimal }) => idAnimal === idSpecie)), []);
 }
+/* data.species.reduce((animals, currentAnimal) => {
+    if (id === currentAnimal.id) {
+      animals.push(currentAnimal);
+      return animals;
+    }
+    return animals;
+  }, []) */
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
