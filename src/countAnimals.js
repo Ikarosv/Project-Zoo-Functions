cd09847d7038c8912d488648eb12c124c11c7540
Ikarosv <ikarovieiraa@gmail.com>
2022-09-29 18:42:25 -0300

    Requisito 5 feito.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals({ specie, sex } = { specie: 'all', sex: 'all' }) {
+  return data.species.reduce((total, currentSpecie) => {
+    if (specie === 'all') {
+      const animal = { ...total };
+      animal[currentSpecie.name] = currentSpecie.residents.length;
+      return animal;
+    }
+    if (currentSpecie.name === specie) {
+      if (sex) {
+        return currentSpecie.residents
+          .filter(({ sex: sexAnimal }) => sexAnimal === sex).length;
+      }
+      return currentSpecie.residents.length;
+    }
+    return total;
+  }, {});
 }
 
 module.exports = countAnimals;
