dfce99ff2355b8f32665ceb142226abc9af3c2e6
Ikarosv <ikarovieiraa@gmail.com>
2022-09-29 16:41:46 -0300

    Requisito 2 feito.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find(({ name }) => name === animal)
+    .residents.every(({ age: ageAnimal }) => ageAnimal >= age);
 }
 
 module.exports = getAnimalsOlderThan;
