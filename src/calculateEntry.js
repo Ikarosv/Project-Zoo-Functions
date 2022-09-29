27ba82f72735a8ab868bb469f8b43cb077a3368d
Ikarosv <ikarovieiraa@gmail.com>
2022-09-29 20:11:29 -0300

    Requisito 8 feito.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
+const entradas = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((totalEntrants, { age }) => {
+    const obj = { ...totalEntrants };
+    if (age < 18) {
+      obj.child += 1;
+    } else if (age >= 18 && age < 50) {
+      obj.adult += 1;
+    } else if (age >= 50) {
+      obj.senior += 1;
+    }
+    return obj;
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  return Object.entries(countEntrants(entrants))
+    .reduce((total, currArr) => total + data.prices[currArr[0]] * currArr[1], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
