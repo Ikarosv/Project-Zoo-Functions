547bc9f78353f6c44d2a9f59de95e25ea8c8195a
Ikarosv <ikarovieiraa@gmail.com>
2022-09-30 15:36:48 -0300

    Modificações

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,13 +5,5 @@ function getSpeciesByIds(...ids) {
   return ids.reduce((animals, idSpecie) => animals
     .concat(data.species.filter(({ id: idAnimal }) => idAnimal === idSpecie)), []);
 }
-/* data.species.reduce((animals, currentAnimal) => {
-    if (id === currentAnimal.id) {
-      animals.push(currentAnimal);
-      return animals;
-    }
-    return animals;
-  }, []) */
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
